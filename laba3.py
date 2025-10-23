e48616f1ff28d6a79b248bc771fb755e077d6c66
Maks Ivats <maxymivats@gmail.com>
2025-10-23 20:45:53 +0300

    lab 2

---
+++ laba3.py
@@ -0,0 +1,18 @@
+# arr = ['максим', 'орест', 'олег', 'артем']
+# index_value_iterator = enumerate(arr)
+# for index, value in index_value_iterator:
+#     print(f"{index} {value}")
+
+
+# def eng_alfavit_generator():
+#     for i in range(ord('A'), ord('Z') + 1):
+#         yield chr(i)
+#
+# alfavit_generator = eng_alfavit_generator()
+# print(list(alfavit_generator))
+
+
+# kvadrat_generator = (i**2 for i in range(1, 21) if i % 2 != 0)
+# print({next(kvadrat_generator)})
+# for x in kvadrat_generator:
+#     print(x)
