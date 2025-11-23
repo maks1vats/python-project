6821ba632dd558057cc8a9ff80a71b33ea186468
Maks Ivats <maxymivats@gmail.com>
2025-11-23 15:57:27 +0200

    Телеграм-бот-перекладач. Курсова робота

--- translatorbot.py
+++ translatorbot.py
@@ -5,6 +5,7 @@ bot = telebot.TeleBot('8342919678:AAGQbzJrhJuGxnW3hoSHaGvyHFYlKAMeuVY')
 
 defaultlang = 'uk'
 
+
 @bot.message_handler(commands=['start'])
 def starttext(message):
     text = (
@@ -18,6 +19,7 @@ def starttext(message):
     )
     bot.reply_to(message, text)
 
+
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def translate(message):
     text = message.text.strip()
@@ -26,9 +28,24 @@ def translate(message):
 
     parts = text.split(maxsplit=1)
 
-    if len(parts) > 1 and len(parts[0] <= 5):
+    if len(parts) > 1 and len(parts[0]) <= 5:
         lang = parts[0].lower()
         texttotrans = parts[1]
+
+    try:
+        translator = GoogleTranslator(source='auto', target=lang)
+        translated_text = translator.translate(texttotrans)
+        answer = (
+            f'Переклад на мову {lang}:\n'
+            f'{translated_text}'
+        )
+
+        bot.reply_to(message, answer)
+
+    except Exception as e:
+        errormes = "Сталася помилка при перекладі! Вкажи вірно код мови, на яку хочеш перекласти свій текст!"
+
+        bot.reply_to(message, errormes)
 
 
 bot.polling(none_stop=True)
